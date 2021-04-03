30ecc91956a9ca5dd025e2745637e3f0ca98d568
Antoine <antoinepascualmail@gmail.com>
2021-04-03 16:21:24 +0200

    rotate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import os
 
 WIDTH, HEIGHT = 900,500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -9,9 +9,21 @@ WHITE = (255,255,255)
 
 FPS = 60
 
+YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.image.load(
+	os.path.join('Assets', 'spaceship_yellow.png')), 90)
+
+
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (55,40))
+
+
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.image.load(
+	os.path.join('Assets', 'spaceship_red.png')), 270)
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (55,40))
 
 def draw_window():
 	WIN.fill(WHITE)
+	WIN.blit(YELLOW_SPACESHIP, (300,100))
+	WIN.blit(RED_SPACESHIP, (100,300))
 	pygame.display.update()
 
 
